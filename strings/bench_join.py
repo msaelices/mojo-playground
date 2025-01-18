f7bac7e71004c0eed33d5e593ed314fc837a48ac
Manuel Saelices <msaelices@gmail.com>
2025-01-18 11:56:06 +0100

    Upload Python version of bench_join.mojo

---
+++ strings/bench_join.py
@@ -0,0 +1,15 @@
+from time import time as now
+
+
+def main():
+    l = []
+    for i in range(100_000):
+        l.append(str(i))
+    start = now()
+    s = ",".join(l)
+    end = now()
+    print("Len: ", len(s), "Time: ", (end - start), "seconds")
+
+
+if __name__ == "__main__":
+    main()
