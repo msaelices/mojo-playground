e67bbefcfb1e5ffcdfd7708ecf2f68bde670e0ce
Manuel Saelices <msaelices@gmail.com>
2026-01-07 00:44:57 +0100

    Fix interop example build scripts to reference playground source
    - Update build.sh scripts to reference ../../../playground/interop/*/mojo_module.mojo
    - Use pixi run mojo consistently in both examples

--- examples/interop/factorial/build.sh
+++ examples/interop/factorial/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mojo build mojo_module.mojo --emit shared-lib -o mojo_module.so
+pixi run mojo build ../../../playground/interop/factorial/mojo_module.mojo --emit shared-lib -o mojo_module.so
--- examples/interop/hello_world/build.sh
+++ examples/interop/hello_world/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mojo build mojo_module.mojo --emit shared-lib -o mojo_module.so
+pixi run mojo build ../../../playground/interop/hello_world/mojo_module.mojo --emit shared-lib -o mojo_module.so
