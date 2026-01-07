c178116c7519c2c8ba491212f30786961abbfbae
Manuel Saelices <msaelices@gmail.com>
2026-01-07 10:55:03 +0100

    Remove now uneeded script

--- run_all_mojo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Script to run all .mojo files and report only failures
-
-find . -name "*.mojo" -type f -not -path "./.pixi/*" | while read file; do
-    if ! mojo "$file" > /dev/null 2>&1; then
-        echo "Failed: $file"
-    fi
-done
